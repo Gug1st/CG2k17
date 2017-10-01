f41fcb3ee746e90397fd2880a2674c2ad2070594
Gug1st <josefinino@hotmail.com>
2017-10-01 19:51:02 +0100

    Add files via upload

--- Proj cg/JS/micromachines.js
+++ Proj cg/JS/micromachines.js
@@ -12,27 +12,40 @@ function render() {
 	renderer.render(scene, camera);
 }
 
-function addBorders(obj, x, y, z) {
+function addBorders() {
 	'use strict';
 	
 	for(var i=0; i< 21; i++) {
-		createRing(48-i*4.5, 0, 53);
-		createRing(47.5-i*4.8, 0, -52.5);
-		createRing(48, 0, 53-i*5);
-		createRing(-48, 0, 53-i*5);
+		createRing(48-i*4.5, 0, 53, "x");
+		createRing(47.5-i*4.8, 0, -52.5, "x");
+		createRing(48, 0, 53-i*5, "x");
+		createRing(-48, 0, 53-i*5, "x");
 	}
 	
 	for(i=0; i<29; i++) {
-		createRing(70-i*4.84, 0, 70.5);
-		createRing(70-i*4.84, 0, -69.5);
-		createRing(70, 0, 70.5-i*5);
-		createRing(-70, 0, 70.5-i*5);
+		createRing(70-i*4.84, 0, 70.5, "x");
+		createRing(70-i*4.84, 0, -69.5, "x");
+		createRing(70, 0, 70.5-i*5, "x");
+		createRing(-70, 0, 70.5-i*5, "x");
 	}
 
 }
 
-function addBorders2(obj, x, y, z) {
+function addWheels(x, y, z) {
+		createRing(x+4.3, y+1, z+1.5, "z");
+		createRing(x-3.4, y+1, z+1.5, "z");
+		createRing(x+4.3, y+1, z-1.5, "z");
+		createRing(x-3.4, y+1, z-1.5, "z");
+}
+
+function addCar(obj, x, y, z) {
 	'use strict';
+	
+	geometry = new THREE.CubeGeometry(7, 1, 7);
+	mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x, y, z);
+	
+	obj.add(mesh);
 }
 
 function addTable(obj, x, y, z) {
@@ -52,22 +65,29 @@ function createTable(x, y, z) {
 	
 	material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: false });
 	
-	addTable(table, 0, 0, 0);
+	addTable(table, x, y, z);
 	
 	scene.add(table);
 	
-	table.position.x = x;
-	table.position.y = y;
-	table.position.z = z;
-	
-	//if (Math.random() < 0.5)
-		addBorders();
-	//else 
-	//	addBorders2();
+	addBorders();
 	
 }
 
-function createRing(x, y, z) {
+function createCar(x, y, z) {
+	'use strict';
+	
+	var car = new THREE.Object3D();
+	
+	material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
+	
+	addCar(car, x, y, z);
+	
+	scene.add(car);
+	
+	addWheels(x, y, z);
+}
+
+function createRing(x, y, z, flag) {
 	'use strict';
 	
 	ring = new THREE.Object3D();
@@ -78,7 +98,26 @@ function createRing(x, y, z) {
 	
 	ring.add(mesh);
 	ring.position.set(x, y, z);
-	ring.rotateX(1.3);
+	if (flag == "x") {
+		ring.rotateX(1.4);
+	} else if (flag == "z") {
+		ring.rotateY(1.5);
+		ring.rotateX(0.55);
+	}
+	scene.add(ring);
+}
+
+function createRingY(x, y, z) {
+	'use strict';
+	
+	ring = new THREE.Object3D();
+	
+	material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: false});
+	geometry = new THREE.TorusGeometry(1, 0.4, 10, 50);
+	mesh = new THREE.Mesh(geometry, material);
+	
+	ring.add(mesh);
+	ring.position.set(x, y, z);
 	
 	scene.add(ring);
 }
@@ -100,6 +139,7 @@ function createScene() {
 	scene.add(new THREE.AxisHelper(10));
 	
 	createTable(0, 0, 0);
+	createCar(-63, 0, 0);
 }
 
 function init() {
